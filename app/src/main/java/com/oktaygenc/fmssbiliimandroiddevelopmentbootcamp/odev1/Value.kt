ee49d1329f845ae4ff5d1a406255168f0cb69b6e
Oktay Genç <gncoktay3866@gmail.com>
2024-12-09 21:59:00 +0300

    add: Completed odev1

---
+++ app/src/main/java/com/oktaygenc/fmssbiliimandroiddevelopmentbootcamp/odev1/Value.kt
@@ -0,0 +1,202 @@
+package com.oktaygenc.fmssbiliimandroiddevelopmentbootcamp.odev1
+
+fun main() {
+    val ilce = "Umraniye"
+    println("ilce: $ilce")
+
+    val kita = "Asya"
+    println("Kita: $kita")
+
+    val faks = "+932212899"
+    println("Faks: $faks")
+
+    val postaKodu = 235658
+    println("Posta Kodu: $postaKodu")
+
+    val instaAdresi = "@oktayGenc"
+    println("Insta Adresi: $instaAdresi")
+
+    val calistigiBolum = "Android Developer"
+    println("Calistigi Bolum: $calistigiBolum")
+
+    val urunMiktari = 7
+    println("Urun Miktari: $urunMiktari")
+
+    val musteriSoyadi = "Genc"
+    println("Musteri Soyadi: $musteriSoyadi")
+
+    val odemeMiktari = 1200
+    println("Odeme Miktari: $odemeMiktari")
+
+    val dogumTarihi = "24.01.2001"
+    println("Dogum Tarihi: $dogumTarihi")
+
+    val borc = 15000
+    println("Borc: $borc")
+
+    val medeniHal = "Bekar"
+    println("Medeni Hal: $medeniHal")
+
+    val videoYorumu = "Eline saglik cok guzel olmus"
+    println("Video Yorumu: $videoYorumu")
+
+    val odemeSaati = "22:15"
+    println("Odeme Saati: $odemeSaati")
+
+    val eftMiktari = 2800
+    println("Eft Miktari: $eftMiktari")
+
+    val satilanMiktar = 14
+    println("Satilan Miktar: $satilanMiktar")
+
+    val telefonModeli = "Samsung"
+    println("Telefon Modeli: $telefonModeli")
+
+    val dergiAdi = "Forbes"
+    println("Dergi Adi: $dergiAdi")
+
+    val basimTarihi = "20.02.2011"
+    println("Basim Tarihi: $basimTarihi")
+
+    val zamMiktari = 3000
+    println("Zam Mikari: $zamMiktari")
+
+    val daireSayisi = 3
+    println("Daire Sayisi: $daireSayisi")
+
+    val enlem = "39°57'49.89"
+    println("Enlem: $enlem")
+
+    val boylam = "32°52'21.67"
+    println("Boylam: $boylam")
+
+    val yemekAdi = "Iskender"
+    println("Yemek Adi: $yemekAdi")
+
+    val urunFiyati = 395
+    println("Urun Fiyati: $urunFiyati")
+
+    val sirket = "FMSS"
+    println("Sirket: $sirket")
+
+    val videoAdi = "Android 101"
+    println("Video Adi: $videoAdi")
+
+    val muzikSuresi = 3
+    println("Muzik Suresi: $muzikSuresi")
+
+    val mekanPuani = 4
+    println("Mekan Puani: $mekanPuani")
+
+    val dosyaAdi = "android.exe"
+    println("Dosya Adi: $dosyaAdi")
+
+    val resimFormati = ".jpeg"
+    println("Resim Formati: $resimFormati")
+
+    val renk = "Mavi"
+    println("Renk: $renk")
+
+    val renkKodu = "#0000ff"
+    println("Renk Kodu: $renkKodu")
+
+    val bilgisayarModeli = "Asus"
+    println("Bilgisayar Modeli: $bilgisayarModeli")
+
+    val ekranBoyutu = 24
+    println("Ekran Boyutu: $ekranBoyutu")
+
+    val kullanimSuresi = "13 saat"
+    println("Kullanim Suresi: $kullanimSuresi")
+
+    val basinc = 3
+    println("Basinc: $basinc")
+
+    val etkinlikGunu = "Cumartesi"
+    println("Etkinlik Gunu: $etkinlikGunu")
+
+    val odemeGunu = "Cuma"
+    println("Odeme Gunu: $odemeGunu")
+
+    val yolculukCikisTarihi = "21.09.2023"
+    println("Yolculuk Cikis Tarihi: $yolculukCikisTarihi")
+
+    val mahalleAdi = "Dudullu"
+    println("Mahalle Adi: $mahalleAdi")
+
+    val otobusHatti = "14B"
+    println("Otobus Hatti: $otobusHatti")
+
+    val kullanilanDakika = 23
+    println("Renk Kodu: $kullanilanDakika")
+
+    val kargoTakipNo = 2399392
+    println("Kargo Takip No: $kargoTakipNo")
+
+    val kuponSuresi = "3 gun"
+    println("Kupon Suresi: $kuponSuresi")
+
+    val kuponKodu = "48AT56Q"
+    println("Kupon Kodu: $kuponKodu")
+
+    val faturaTarihi = "13.11.2024"
+    println("Fatura Tarihi: $faturaTarihi")
+
+
+
+    println("---------------------- IKINCI YOL ----------------------")
+
+    //Alternatif yol
+
+    fun yazdir(kategori: String, deger: Any) {
+        println("$kategori: $deger")
+    }
+    yazdir("Ilce", "Umraniye")
+    yazdir("Kita", "Asya")
+    yazdir("Faks", "+932212899")
+    yazdir("Posta Kodu", 235658)
+    yazdir("Instagram Adresi", "@oktayGenc")
+    yazdir("Calistigi Bolum", "Android Developer")
+    yazdir("Urun Miktari", 7)
+    yazdir("Musteri Soyadi", "Genc")
+    yazdir("Odeme Miktari", 1200)
+    yazdir("Dogum Tarihi", "24.01.2001")
+    yazdir("Borc", 15000)
+    yazdir("Medeni Hal", "Bekar")
+    yazdir("Video Yorumu", "Eline saglik cok guzel olmus")
+    yazdir("Odeme Saati", "22:15")
+    yazdir("EFT Miktari", 2800)
+    yazdir("Satilan Miktar", 14)
+    yazdir("Telefon Modeli", "Samsung")
+    yazdir("Dergi Adi", "Forbes")
+    yazdir("Basim Tarihi", "20.02.2011")
+    yazdir("Zam Miktari", 3000)
+    yazdir("Daire Sayisi", 3)
+    yazdir("Enlem", "39°57'49.89")
+    yazdir("Boylam", "32°52'21.67")
+    yazdir("Yemek Adi", "Iskender")
+    yazdir("Urun Fiyati", 395)
+    yazdir("Sirket", "FMSS")
+    yazdir("Video Adi", "Android 101")
+    yazdir("Muzik Suresi", 3)
+    yazdir("Mekan Puani", 4)
+    yazdir("Dosya Adi", "android.exe")
+    yazdir("Resim Formati", ".jpeg")
+    yazdir("Renk", "Mavi")
+    yazdir("Renk Kodu", "#0000ff")
+    yazdir("Bilgisayar Modeli", "Asus")
+    yazdir("Ekran Boyutu", 24)
+    yazdir("Kullanim Suresi", "13 saat")
+    yazdir("Basinc", 3)
+    yazdir("Etkinlik Gunu", "Cumartesi")
+    yazdir("Odeme Gunu", "Cuma")
+    yazdir("Yolculuk Cikis Tarihi", "21.09.2023")
+    yazdir("Mahalle Adi", "Dudullu")
+    yazdir("Otobus Hatti", "14B")
+    yazdir("Kullanilan Dakika", 23)
+    yazdir("Kargo Takip No", 2399392)
+    yazdir("Kupon Suresi", "3 gun")
+    yazdir("Kupon Kodu", "48AT56Q")
+    yazdir("Fatura Tarihi", "13.11.2024")
+
+}
